7d9581c458be961fe6409b8c4c856804b159d462
KIRUBA SANKAR <kirubasankars@fedora33.verizon.net>
2020-09-27 11:40:28 -0400

    updated dist folder

--- dist/appy1/module.js
+++
@@ -1,7 +0,0 @@
-import routes from './routes'
-import store from './store'
-
-export default {
-    routes,
-    store
-}
--- dist/appy1/routes/index.js
+++
@@ -1,9 +0,0 @@
-
-export default [
-    {       
-      path: '/appy/list/databases',
-      name: 'appy.ListDatabases',
-      component: () => import(/* webpackChunkName: "appy" */ '../views/Test.vue')   
-    }
-  ]
-  
